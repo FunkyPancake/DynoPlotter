4b8fb12a051b8904cfa004fd6030c2d0e7a53de5
FunkyPancake <43291121+FunkyPancake@users.noreply.github.com>
2024-02-20 02:25:16 +0100

    mvp (#1)

--- MainWindow.py
+++ MainWindow.py
@@ -1,41 +1,46 @@
 import logging
 
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
-from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
-
 from PySide6 import QtWidgets
-from PySide6.QtGui import QAction
-from PySide6.QtWidgets import QMainWindow, QFileDialog
+from PySide6.QtGui import QAction, QStandardItemModel, QStandardItem
+from PySide6.QtWidgets import QMainWindow, QFileDialog, QWidget, QListView, QCheckBox, QVBoxLayout
+from PySide6.QtCore import Qt
+from matplotlib.widgets import Cursor
 
+from CustomPlot import CustomPlot
 from LogParser import LogParser
 from SignalProcessor import SignalProcessor
 from ui_mainwindow import Ui_MainWindow
 
 
-class MplCanvas(FigureCanvasQTAgg):
+class OtherWindow(QWidget):
+    def __init__(self, parser, item_changed_event):
+        super(OtherWindow, self).__init__()
 
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig, ax1 = plt.subplots()
-        plt.grid(visible=True, which='both')
-        self.fig = fig
-        self.ax1 = ax1
-        self.ax2 = ax1.twinx()
-        # fig = Figure(figsize=(width, height), dpi=dpi)
-        # self.axes = fig.add_subplot(111)
-        super(MplCanvas, self).__init__(fig)
-
-    def add_plot(self, x, y):
-        self.plots.append((x, y))
+        layout = QVBoxLayout(self)
+        list_view = QListView()
+        self.model = QStandardItemModel()
+        for name in parser.headers:
+            item = QStandardItem(name)
+            item.setCheckable(True)
+            item.setCheckState(Qt.Unchecked)
+            self.model.appendRow(item)
+        list_view.setModel(self.model)
+        layout.addWidget(list_view)
+        self.model.itemChanged.connect(item_changed_event)
+        self.setLayout(layout)
 
 
 class MainWindow(QMainWindow):
+    X_NAME = 'STAT_MOTORDREHZAHL_WERT'
+
     def __init__(self, logger: logging.Logger):
         super().__init__()
+        self.ow = None
         self.sc = None
         self.is_opened = False
         self.logger = logger
         self.log_parser = LogParser(logger)
+        self.sc = CustomPlot()
 
         self.m_ui = Ui_MainWindow()
         self.m_ui.setupUi(self)
@@ -50,44 +55,53 @@ class MainWindow(QMainWindow):
         menubar.addAction(self.save_action)
 
         self.select_action = QAction("&Select", self)
-        self.select_action.triggered.connect(self.select_handler)
+        self.select_action.triggered.connect(self.on_select_clicked)
         menubar.addAction(self.select_action)
 
         self.select_action.setEnabled(False)
         self.save_action.setEnabled(False)
 
-    def select_handler(self):
-        pass
+    def closeEvent(self, event):
+        self.ow.close()
+        self.close()
+
+    def on_select_clicked(self):
+        self.ow = OtherWindow(self.log_parser, self.on_item_changed)
+        self.ow.show()
+        self.logger.debug('Select items button clicked.')
+
+    def on_item_changed(self, item: QStandardItem):
+        name = item.text()
+        if item.checkState() == Qt.Checked:
+            x_axis = self.log_parser.get_data(self.X_NAME)
+            y_axis = self.log_parser.get_data(name)
+            self.sc.add_plot(name, x_axis, y_axis)
+        else:
+            self.sc.delete_plot(name)
+        self.logger.debug(item.text())
 
     def open_handler(self):
-        QFileDialog.getOpenFileName(self)
-        self.log_parser.parse('./log.csv')
+        self.logger.debug('Open file button clicked.')
+
+        filename, selected_filter = QFileDialog.getOpenFileName(self)
+        self.log_parser.parse(filename)
         self.select_action.setEnabled(True)
         self.save_action.setEnabled(True)
 
-        self.sc = MplCanvas(self, width=5, height=4, dpi=100)
+        self.logger.debug(f'{filename} opened successfully.')
+
         sp = SignalProcessor(self.logger, self.log_parser)
-        x_axis = self.log_parser.get_data('STAT_MOTORDREHZAHL_WERT')
+        x_axis = self.log_parser.get_data(self.X_NAME)
         y_axis = self.log_parser.get_data('STAT_MOTORMOMENT_AKTUELL_WERT')
-        self.sc.ax1.plot(x_axis, y_axis, 'g-')
-        self.sc.ax2.plot(x_axis, sp.calc_power(), 'r-')
-        self.sc.ax1.set_xlabel('RPM')
-        self.sc.ax1.set_ylabel('Torque [Nm]', color='g')
-        self.sc.ax2.set_ylabel('Power [kW]', color='r')
+        self.sc.add_base_plots(x_axis, y_axis,  sp.calc_power())
 
         self.setCentralWidget(self.sc)
 
-        # self.m_ui.widget = sc
-
     def save_handler(self):
-        print(self.log_parser.headers)
         filename = 'plot.png'
-        filename, selected_filter = QFileDialog.getSaveFileName(self, 'Save',filename, filter='PNG Files (*.png)')
-        if not filename.endswith('.png'):
-            filename = filename + '.png'
-        self.sc.fig.savefig(filename)
-        # self.m_ui.widget = sc
-
-#
-# x = LogParser(logger,'./log.csv')
-# x.parse()
+        filename, selected_filter = QFileDialog.getSaveFileName(self, 'Save', filename, filter='PNG Files (*.png)')
+        if filename != "":
+            self.sc.fig.savefig(filename)
+            self.logger.debug(f'Save plot to {filename}.')
+        else:
+            self.logger.debug('Saving of the file aborted.')
--- main.py
+++ main.py
@@ -7,8 +7,12 @@ from MainWindow import MainWindow
 
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+        datefmt="%d/%b/%Y %H:%M:%S",
+        stream=sys.stdout)
     logger.setLevel(logging.DEBUG)
-    logger.addHandler(logging.StreamHandler(sys.stdout))
     app = QApplication(sys.argv)
     window = MainWindow(logger)
     window.show()
